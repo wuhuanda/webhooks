e41ddf6ff5cd5244460a231e8252d2d71cbe45f3
w6a <583783099@qq.com>
2019-12-07 17:21:38 +0800

    fix: 修正项目路径

--- cicd.sh
+++ cicd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WORK_PATH='/usr/projects/vue-back'
+WORK_PATH='~/workplace/NeteaseCloudMusicApi'
 cd $WORK_PATH
 echo "清理后台代码"
 git reset --hard origin/master
@@ -8,15 +8,15 @@ echo "拉取后台最新代码"
 git pull origin master
 
 
-WORK_PATH='/usr/projects/vue-front'
-cd $WORK_PATH
-echo "清理前台代码"
-git reset --hard origin/master
-git clean -f
-echo "拉取前台最新代码"
-git pull origin master
-echo "打包前台最新代码"
-npm run build
+# WORK_PATH='/usr/projects/vue-front'
+# cd $WORK_PATH
+# echo "清理前台代码"
+# git reset --hard origin/master
+# git clean -f
+# echo "拉取前台最新代码"
+# git pull origin master
+# echo "打包前台最新代码"
+# npm run build
 
 cd /usr/projects
 echo "删除老资源"
