bfd5c49b08cc14110b005c96537b888ec217460d
w6a <583783099@qq.com>
2020-01-02 18:25:31 +0800

    新增前端

---
+++ wyy-cloud-music-fe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORK_PATH='/workplace/wyy-cloud-music'
+cd $WORK_PATH
+echo "清理代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取最新代码"
+git pull origin master
+echo "开始构建镜像"
+docker build -t wyy-cloud-music:1.0 .
+echo "删除旧容器"
+docker stop wyy-cloud-music-container
+docker rm wyy-cloud-music-container
+echo "启动新容器"
+docker container run -p 8000:8000 -d --name wyy-cloud-music-container -d wyy-cloud-music:1.0
